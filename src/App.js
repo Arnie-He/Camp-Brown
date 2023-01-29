9f391b8fc68a5995ab7fe641f84ddaf9704ececf
jdai1 <julian.dai@gmail.com>
2023-01-28 23:24:11 -0500

    UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { Icon, IconButton } from '@mui/material';
 
 function App() {
+  const [recs, setRecs] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App" 
+      style={{
+        position: "absolute",
+        top: 0, left: 0,
+        backgroundColor: "#eba13f", 
+        height: "100vh",
+        width: "100%",
+        }}
+    >
+      <AppBar sx={{color: "#eba13f", backgroundColor: "#4a2c04"}}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Camp@Brown
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Paper sx={{mt: 12, mr: 4, ml: 4, height: "80%", background: "#4a2c04"}}> 
+        <Grid container sx={{p: 2, color:'white', height: '100%', alignItems: 'center'}}>
+          <Grid item xs={1}>
+            <IconButton sx={{color:'white'}}><NavigateBeforeIcon /></IconButton>
+          </Grid>
+          <Grid item xs={10}>
+            <Typography variant="h4" sx={{fontFamily: 'sans-serif', fontWeightLight: 300}}>Hack @ Brown</Typography>
+            <Typography>Instructor:</Typography>
+            <Typography>Description:</Typography>
+            <Typography>Mutuals:</Typography>
+          </Grid>
+          <Grid item xs={1}>
+            <Box sx={{flexGrow: 1, ml: 5}}>
+              <IconButton sx={{color:'white'}}><NavigateNextIcon /></IconButton>
+            </Box>
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
   );
 }
